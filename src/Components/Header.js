b73ed7c1ec17f7f7cbd7603705d9bbdc59ec052f
storngfornt <au0038173@gmail.com>
2023-10-22 19:37:13 +0000

    tut

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex items-center justify-center bg-white  h-20 gap-5 text-black'>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+      <p>Home</p>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+import Header from '@/Components/Header'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+   <Header/>
+  )
+}
